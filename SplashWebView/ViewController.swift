e104b93a2e00dfd8ef7c0d26976f83259aa98a0c
Eduardo TC <eduar2tc@gmail.com>
2020-03-17 17:44:09 -0600

    Initial Commit

---
+++ SplashWebView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SplashWebView
+//
+//  Created by Edu on 17/03/20.
+//  Copyright © 2020 Edu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
